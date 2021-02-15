ef55d83cd072751a2af54e58cfa561241d4a86cf
wrenrobertson <jrr0038@uah.edu>
2021-02-15 15:20:22 -0600

    Integrates timer now

--- src/MainGame.java
+++ src/MainGame.java
@@ -15,35 +15,15 @@ public class MainGame
         JFrame window = trial.createWindow();  // create the window JFrame
         GridMap map = new GridMap(600, 600,20,20);
 
-        JPanel timerPanel = new JPanel();
-
         Icon icon1 = new ImageIcon("play.png");
-        JButton playButton = new JButton(icon1);
-        playButton.setVisible(true);
-
-        Dimension size = playButton.getPreferredSize();
-        //playButton.setBounds(500, 180, size.width, size.height);
-        playButton.setVisible(true);
-
-
         Icon icon2 = new ImageIcon("save.png");
         JButton saveButton = new JButton(icon2);
         saveButton.setVisible(true);
-
-        //SimTimer gameTimer = new SimTimer();
-        //gameTimer.startTimer();
-
-        timerPanel.add(saveButton);
-        timerPanel.add(playButton);
-        //timerPanel.setLayout(null);
-        //timerPanel.setLocation(150,10000);
-        //timerPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
-        //timerPanel.add(gameTimer);
+        TimePanel timer = new TimePanel();
 
         window.add(map);
-        window.add(timerPanel);
-        //window.add(playButton);
-        //window.add(saveButton);
+        window.add(timer);
+        window.add(saveButton);
         window.pack();
     }
 }
