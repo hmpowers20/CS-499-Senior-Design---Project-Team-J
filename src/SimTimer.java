d9b7b524641bb131da747e643155a80ac5783034
wrenrobertson <jrr0038@uah.edu>
2021-02-15 15:20:46 -0600

    Yay the timer works as long as you don't pause it

--- src/SimTimer.java
+++ src/SimTimer.java
@@ -3,44 +3,66 @@ public class SimTimer {
     boolean shouldCount=false;
     int int_sec=0;
     int int_min=0;
-    int int_mil=0;
+    int int_day=0;
+    int int_hrs = 0;
+    long resumeTime;
+    boolean running;
+    TimePanel tPanel;
 
-    public SimTimer() {
+    public SimTimer(TimePanel tPanel) {
+        resumeTime = 0;
+        this.tPanel = tPanel;
+        running = true;
     }
 
-    public static void main(String[] args) {
-        TimeFrame t = new TimeFrame();
-        JFrame f = new JFrame("Timer");
-        f.setSize(300,200);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLocationRelativeTo(null);
-        f.getContentPane().add(new TimeFrame());
-        f.setVisible(true);
+    public SimTimer(long resumeTime, TimePanel tPanel) {
+        this.resumeTime = resumeTime;
+        this.tPanel = tPanel;
+        running = true;
     }
 
-    public void count(){
+    public void startTimer(){
         Thread thread = new Thread(new Runnable() {
+            long startTime = System.currentTimeMillis() - resumeTime;
             @Override
             public void run() {
+                try {
+                    while (true) {
+                        if (running) {
+                            Thread.sleep(1000);
+                            long now = System.currentTimeMillis() - startTime;
+                            int total = (int) (now / 1000);
+                            int_day = total / 86400;
+                            total = total - int_day * 86400;
+                            int_hrs = total / 3600;
+                            total = total - int_hrs * 3600;
+                            int_min = total / 60;
+                            total = total - int_min * 60;
+                            int_sec = total;
+
+
+                            tPanel.update(int_sec, int_min, int_hrs, int_day);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 long now = System.currentTimeMillis();
                 while(true){
                     if(shouldCount){
-                        if(System.currentTimeMillis()-now>=100){
-                            now=System.currentTimeMillis();
-                            int_mil++;
-                            if(int_mil>9){
-                                int_mil=0;
-                                int_sec++;
-                                if(int_sec>=60){
-                                    int_sec=1;
-                                    int_min++;
-                                }
-                            }
-                        }
+
                     }
                 }
             }
         });
         thread.start();
+    }
+    public void pause() {
+        if (running) {
+            running = false;
+        }
+        else {
+            running = true;
+        }
     }
 }
