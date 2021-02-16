3dd1627175c3d2111cf61b20340b88e7af44a2ed
wrenrobertson <jrr0038@uah.edu>
2021-02-16 13:06:39 -0600

    Now has saving functions

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 /**
@@ -13,14 +15,15 @@ public class MainGame
      */
     public static void main (String[] args) throws IOException, InterruptedException {
         JFrame window = trial.createWindow();  // create the window JFrame
-        GridMap map = new GridMap(30, 30);
+        GridMap map = new GridMap(600, 600,20,20);
 
         Icon icon1 = new ImageIcon("play.png");
         Icon icon2 = new ImageIcon("save.png");
         JButton saveButton = new JButton(icon2);
         saveButton.setVisible(true);
         TimePanel timer = new TimePanel();
-      
+        saveButton.addActionListener(new saveAll(timer));
+
         window.add(map);
         window.add(timer);
         window.add(saveButton);
@@ -28,3 +31,15 @@ public class MainGame
     }
 }
 
+class saveAll implements ActionListener {
+    TimePanel timer;
+    SaveSession save;
+    public saveAll(TimePanel timer) {
+        this.timer = timer;
+        this.save = new SaveSession(timer);
+    }
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        save.saveTimer();
+    }
+}
