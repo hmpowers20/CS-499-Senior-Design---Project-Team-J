3d3618e261d9e59d3ebcbe2ed4067d7d9d153efa
wrenrobertson <jrr0038@uah.edu>
2021-02-17 15:55:32 -0600

    Plant

---
+++ src/Plant.java
@@ -0,0 +1,32 @@
+public class Plant {
+    int radius;
+    int number_of_seeds;
+    int size;
+    int growing_speed;
+    int energy;
+    int x;
+    int y;
+    public Plant() {
+
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    int getEnergy() {
+        return energy;
+    }
+
+    public void pause() {
+
+    }
+
+    public void resume() {
+
+    }
+}
