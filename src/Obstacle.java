550453d33503092210a016ce6d62092684e5bd86
wrenrobertson <jrr0038@uah.edu>
2021-02-17 17:34:00 -0600

    3D object for a 2D plain yay

---
+++ src/Obstacle.java
@@ -0,0 +1,8 @@
+public class Obstacle {
+    int x, y, h;
+    public Obstacle(int x, int y, int h) {
+        this.x = x;
+        this.y = y;
+        this.h = h; //Why do these have height; I do not understand
+    }
+}
