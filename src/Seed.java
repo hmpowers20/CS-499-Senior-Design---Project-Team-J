d1d4eedfd8a61411665c4864fd0882c1df852d8d
wrenrobertson <jrr0038@uah.edu>
2021-02-17 15:47:39 -0600

    Early version of a seed, it'll evolve

---
+++ src/Seed.java
@@ -0,0 +1,15 @@
+public class Seed {
+    int plantRadius;
+    int simSpeed;
+    int speed;
+    int probability;
+    public Seed() {
+        simSpeed = 1;
+    }
+    public void pause() {
+
+    }
+    public void resume() {
+
+    }
+}
