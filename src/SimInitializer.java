f20e7e8deab43198541b1bb5ef226225fde09c73
wrenrobertson <jrr0038@uah.edu>
2021-02-17 23:29:19 -0600

    XML parsing in progress!

--- src/SimInitializer.java
+++ src/SimInitializer.java
@@ -1,6 +1,40 @@
+import java.io.File;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
 public class SimInitializer {
     int idNumber;
+    File inputFile;
     public SimInitializer() {
+        idNumber = 0;
+    }
 
+    public void initialize() throws ParserConfigurationException {
+
+        //JavatPoint code here we goooo
+        inputFile = new File("LifeSimulation01.xml");
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = null;
+        try {
+            doc = db.parse(inputFile);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        doc.getDocumentElement().normalize();
+
+        NodeList plantList = doc.getElementsByTagName("Plant");
+        NodeList predatorList = doc.getElementsByTagName("Predator");
+        NodeList obstacleList = doc.getElementsByTagName("Obstacle");
+        NodeList grazerList = doc.getElementsByTagName("Grazer");
+
+        for (int i = 0; i < plantList.getLength(); i++) {
+            Node plant = plantList.item(i);
+        }
     }
 }
