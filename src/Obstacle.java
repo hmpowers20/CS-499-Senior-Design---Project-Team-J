4c31e384be26d186441e656ee8a33bcd6b679fd8
wrenrobertson <jrr0038@uah.edu>
2021-02-22 10:34:15 -0600

    Got some upgrades

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,8 +1,25 @@
 public class Obstacle {
-    int x, y, h;
-    public Obstacle(int x, int y, int h) {
+    int x, y, h, d;
+    public Obstacle(int x, int y, int d, int h) {
         this.x = x;
         this.y = y;
+        this.d = d;
         this.h = h; //Why do these have height; I do not understand
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+    int getH() {
+        return h;
+    }
+
+    int getD() {
+        return d;
     }
 }
