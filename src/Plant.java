b87a0f7d8a078b52737987531b13e3180a242ad6
wrenrobertson <jrr0038@uah.edu>
2021-04-06 00:06:26 -0500

    Plant

--- src/Plant.java
+++ src/Plant.java
@@ -1,19 +1,35 @@
 public class Plant extends Actor  {
-    int radius, maxSeeds, maxSeedDistance;
+    int maxSeeds, maxSeedDistance, max_size;
     float viability, rate;
+    float radius;
+    boolean edible = true; //This is just sort of a hack to allow the plant to be eaten without out the exact mechanics
+    int counter = 60; //This too
 
-    public Plant(float rate, int radius, int maxSeeds, int maxSeedDistance, float viability, float x, float y) {
+    public Plant(float rate, int diameter, int max_size, int maxSeeds, int maxSeedDistance, float viability, float x, float y) {
         this.rate = rate;
-        this.radius = radius;
+        this.radius = diameter / 2;
         this.maxSeeds = maxSeeds;
         this.maxSeedDistance = maxSeedDistance;
         this.viability = viability;
         this.x = x;
         this.y = y;
+        this.max_size = max_size;
     }
 
     @Override
     public void Update(MainGameModel model) {
+        if (radius < max_size) {
+            radius += rate / 60;
+        }
+    }
+
+    public void eaten() {
+        if (counter < 60) {
+            counter++;
+        }
+        else {
+            edible = false;
+        }
 
     }
 
@@ -21,10 +37,10 @@ public class Plant extends Actor  {
         Seed seed;
         //Do code here that generates a random number of seeds <= maxSeeds
         //They appear at a distance <= maxSeedDistance
-        seed = new Seed(rate, radius, maxSeeds, maxSeedDistance, viability);
+        //seed = new Seed(rate, radius, maxSeeds, maxSeedDistance, viability);
     }
 
-    int getEnergy() {
+    float getEnergy() {
         return energy;
     }
 }
